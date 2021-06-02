cf93d381af13e43b2470f7774dd631fa30a22581
First Dev <mohamadhenda@gmail.com>
2021-06-02 20:56:40 +0100

    init

--- command.ts
+++ command.ts
@@ -3,7 +3,7 @@ import { Message } from 'discord.js'
 export default (msg: Message) => {
     const command = msg.content.split(' ')[0].substr(1)
     try {
-        require(`./commands/${command}`).default( msg)
+        require(`./commands/${command}`).default(msg)
     } catch (error) {
         if (error.code === 'MODULE_NOT_FOUND') {
             msg.channel.send(`${process.env.PREFIX}${command} is not a valid command !`)
